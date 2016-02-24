b043da1b5ae80e5e8df09fdac1c0931402a1dfd0
Tigerkin <pxhszcn@hotmail.com>
2016-02-24 13:01:10 +0800

    Init commit.

---
+++ AirTrade.DataCenter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AirTrade.DataCenter.Startup))]
+namespace AirTrade.DataCenter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
